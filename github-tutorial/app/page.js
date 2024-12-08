6dd02f7c05fcccac155be4f030ae1367f388461c
Sagnik <deydsagnik32@gmail.com>
2024-12-09 03:10:30 +0530

    Added new components on website

--- github-tutorial/app/page.js
+++ github-tutorial/app/page.js
@@ -1,10 +1,19 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import Navbar from '@/components/Navbar'
 import React from 'react'
+
 
 const TestPage = () => {
   return (
     <div>
       This is just a test for the following test for the using github branches and all:
-      
+
+      <Navbar />
+
+      <Header />
+
+      <Hero />
 
 
     </div>
---
+++ github-tutorial/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      This is just a navbart
+    </div>
+  )
+}
+
+export default Navbar
